f565736df9b2478bb3825bff056e475816be8779
Li Kun <lixx4136@d.umn.edu>
2017-01-29 01:01:03 -0600

    add requirement diff:m

--- src/leet01/pkg28/Leet0128.java
+++ src/leet01/pkg28/Leet0128.java
@@ -4,7 +4,13 @@
  * and open the template in the editor.
  */
 package leet01.pkg28;
-
+/*
+Given a string, find the length of the longest substring without repeating characters.
+Examples:
+Given "abcabcbb", the answer is "abc", which the length is 3.
+Given "bbbbb", the answer is "b", with the length of 1.
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         int stringLength=s.length();
